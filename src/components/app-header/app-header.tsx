aa33b6148be4951cab2f2bff5987569976704a9a
gnkm <genki.matsunaga@gmail.com>
2026-06-27 13:39:25 +0900

    feat: ヘッダー作成

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,9 @@
+export function AppHeader() {
+  return (
+    <header className="flex items-center justify-between">
+      <h1 className="text-2xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-50">
+        Multi Timer
+      </h1>
+    </header>
+  );
+}
