06ded24f83647448509492da4c5ecc815232baea
gnkm <genki.matsunaga@gmail.com>
2026-06-27 14:42:56 +0900

    feat: タイマーをストア作成のものに差し替え

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,30 +1,11 @@
 import { AppHeader } from "@/components/app-header/app-header";
 import { AppShell } from "@/components/layout/app-shell";
 import { TimerCardList } from "@/components/timer/timer-card-list";
-import type { Timer } from "@/types/timer";
-
-const timers: Timer[] = [
-  {
-    id: "1",
-    initialSeconds: 60,
-    remainingSeconds: 60,
-    status: "stopped",
-  },
-  {
-    id: "2",
-    initialSeconds: 120,
-    remainingSeconds: 120,
-    status: "stopped",
-  },
-  {
-    id: "3",
-    initialSeconds: 180,
-    remainingSeconds: 150,
-    status: "stopped",
-  },
-];
+import { useTimerStore } from "@/stores/timer-store";
 
 export function App() {
+  const timers = useTimerStore((state) => state.timers);
+
   return (
     <AppShell>
       <AppHeader />
